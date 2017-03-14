536c1044108b1049c917a8f5f7c03ce8951849d7
Argeta Morina <argetamorina@gmail.com>
2017-03-14 12:26:59 +0100

    Added Multiples & Multiply assignment

---
+++ Python-Django/Python/Multiples/sample.py
@@ -0,0 +1,12 @@
+number = 1
+def function():
+	for number in range(1,1000):
+ 		print number + 2
+function()
+
+def function2():
+	x = 5
+	for x in xrange(5,1000000):
+		print x*5
+
+function2()
---
+++ Python-Django/Python/Multiply/sample.py
@@ -0,0 +1,5 @@
+a = [2,4,10,16] 
+def multiply(a):
+	for x in xrange(0, 4):
+		print a[x] * 5
+multiply(a)
